d26136e159be45738a161ca330d02e7ef7242e59
Sumit Sahu <99080079+SumitSahu44@users.noreply.github.com>
2025-02-03 23:26:09 +0530

    Update apidata.js

--- controllers/apidata.js
+++ controllers/apidata.js
@@ -25,7 +25,7 @@ function apiUsers()
         
                      }
                },
-
+              // delete user using their name 
              async deleteOne(req,res)
               {
                  const userName = req.params.name;
