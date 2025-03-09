951e9cb9c707a71eada454eec77648ee2d1101a5
Sumit Sahu <99080079+SumitSahu44@users.noreply.github.com>
2025-03-09 22:57:15 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const route = express.Router();
 const apiUsers = require('../controllers/apidata')
 
 
-
+// routes 
 route.get('/users', apiUsers().users)
 // route.get('/user/:name', apiUsers().userOne)
 route.get('/user/delete/:name', apiUsers().deleteOne)
