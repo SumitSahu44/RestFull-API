24a6ab33ddc6aa570e1a9756969fd9663831af8b
Sumit Sahu <99080079+SumitSahu44@users.noreply.github.com>
2025-08-01 22:55:28 +0530

    Update web.js

--- routes/web.js
+++ routes/web.js
@@ -3,8 +3,6 @@ const route = express.Router()
 const userRegisterController = require('../controllers/registerController')
 
 
-
-
 route.get('/', userRegisterController().index)
 route.post('/register', userRegisterController().register)
 route.post('/signin, userRegisterController().login)
